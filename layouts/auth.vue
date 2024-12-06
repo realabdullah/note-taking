f2fd0fa05c4ab169651ed11fa4fa48decdfc84df
realabdullah <abdulodesanmi@gmail.com>
2024-12-06 13:20:23 +0100

    (chore): project setup

---
+++ layouts/auth.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="h-screen w-screen bg-neutral-100 overflow-auto scheme-light-dark flex items-center justify-center p-4 md:p-8 lg:p-16"
+  >
+    <div
+      class="w-full max-w-[540px] mx-auto bg-white min-h-[623px] rounded-12 border border-neutral-200 shadow-large overflow-auto px-4 py-10 md:p-12 lg:p-12"
+    >
+      <div class="mb-6 flex items-center justify-center">
+        <UIcon name="i-custom-logo" size="30" />
+      </div>
+
+      <NuxtPage />
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  icon: {
+    customCollections: [{
+      prefix: 'custom',
+      dir: './assets/icons'
+    }]
+  },
+  fonts: {
+    assets: {
+      prefix: '/_fonts/',
+    },
+    families: [
+      { name: 'Source Code Pro', provider: 'local' },
+      { name: 'Inter', provider: 'local' },
+      { name: 'Noto Serif', provider: 'local' }
+    ],
+    defaults: {
+      weights: [400, 500, 600, 700],
+      styles: ['normal'],
+    },
+    experimental: {
+      processCSSVariables: true
+    }
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+})
